a690d881e0f2d38a82e692feb6f79a72a970a824
Minyong Ha <pol5412@naver.com>
2025-11-25 18:57:57 +0900

    [Feat] Event 버튼 추가
    - 김준호의 작품

--- app/src/main/java/com/example/temidummyapp/MainActivity.java
+++ app/src/main/java/com/example/temidummyapp/MainActivity.java
@@ -186,9 +186,9 @@ public class MainActivity extends AppCompatActivity implements OnGoToLocationSta
         }
 
         // [1124] 하민용 임시 작업 textViewWeMeet 클릭 → QR 안내 화면 이동
-        TextView textViewWeMeet = findViewById(R.id.textViewWeMeet);
-        if (textViewWeMeet != null) {
-            textViewWeMeet.setOnClickListener(new View.OnClickListener() {
+        CardView cardEvent = findViewById(R.id.card_main_event);
+        if (cardEvent != null) {
+            cardEvent.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(MainActivity.this, EventQRActivity.class);
